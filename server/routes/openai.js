93a5250cca4eb22288d1b5d211bc0dbcde09286f
Joshua <joshuaxdmb@gmail.com>
2025-03-22 23:56:31 -0400

    refactor(openai): remove unused code and DRY'd

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -1,62 +1,69 @@
-import express from 'express';
-import axios from 'axios';
-import { openai } from '../index.js';
-import * as dotenv from 'dotenv';
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
-import { JSONLoader } from 'langchain/document_loaders/fs/json';
-import { TextLoader } from 'langchain/document_loaders/fs/text';
-import { RetrievalQAChain } from 'langchain/chains';
-import { HNSWLib } from 'langchain/vectorstores/hnswlib';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import { ContextualCompressionRetriever } from 'langchain/retrievers/contextual_compression';
-import { LLMChainExtractor } from 'langchain/retrievers/document_compressors/chain_extract';
-import { OpenAI } from 'langchain';
+import express from 'express'
+import axios from 'axios'
+import { openai } from '../index.js'
+import * as dotenv from 'dotenv'
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory'
+import { JSONLoader } from 'langchain/document_loaders/fs/json'
+import { TextLoader } from 'langchain/document_loaders/fs/text'
+import { RetrievalQAChain } from 'langchain/chains'
+import { HNSWLib } from 'langchain/vectorstores/hnswlib'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { ContextualCompressionRetriever } from 'langchain/retrievers/contextual_compression'
+import { LLMChainExtractor } from 'langchain/retrievers/document_compressors/chain_extract'
+import { OpenAI } from 'langchain'
 
-dotenv.config();
+dotenv.config()
+
+const chatConfig = {
+  model: 'gpt-3.5-turbo',
+  messages: [
+    {
+      role: 'user',
+      content: `Finish my thought (do not include my prompt): ${text}`,
+    },
+  ],
+  temperature: 0.5,
+  max_tokens: 2048,
+}
 
 const loader = new DirectoryLoader('./routes/context', {
   '.json': (path) => new JSONLoader(path),
   '.txt': (path) => new TextLoader(path),
-});
+})
 
 const call = async (text) => {
-  const docs = await loader.load();
-  console.log('Docs', { docs });
+  const docs = await loader.load()
+  console.log('Docs', { docs })
 
   const model = new OpenAI({
     modelName: 'gpt-3.5-turbo',
     openAIApiKey: process.env.OPENAI_API_KEY,
     temperature: 0.5,
-  });
+  })
 
-  const basePrompt = ' ';
-  const baseCompressor = LLMChainExtractor.fromLLM(model);
-  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+  const basePrompt = ' '
+  const baseCompressor = LLMChainExtractor.fromLLM(model)
+  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings())
   const retriever = new ContextualCompressionRetriever({
     baseCompressor,
     baseRetriever: vectorStore.asRetriever(),
-  });
+  })
 
-  const chain = RetrievalQAChain.fromLLM(model, retriever);
-  const prompt = basePrompt.concat(text);
+  const chain = RetrievalQAChain.fromLLM(model, retriever)
+  const prompt = basePrompt.concat(text)
   const res = await chain.call({
     query: prompt,
-  });
-  return res;
-};
+  })
+  return res
+}
 
-const router = express.Router();
+const router = express.Router()
 
 router.post('/text', async (req, res) => {
   try {
-    const { text, activeChatId } = req.body;
-    const response = await call(text);
-    // const response = await openai.createChatCompletion({
-    //     model: "gpt-3.5-turbo",
-    //     messages: [{"role": "system", "content": "You are a helpful assistant. Your name is Jarvis."}, {role: "user", content: text}],
-    //     temperature:0.5,
-    //     max_tokens:2048,
-    // })
+    const { text, activeChatId } = req.body
+    const response = await call(text)
+
     await axios.post(
       `https://api.chatengine.io/chats/${activeChatId}/messages/`,
       {
@@ -69,38 +76,22 @@ router.post('/text', async (req, res) => {
           'User-Secret': process.env.BOT_USER_SECRET,
         },
       }
-    );
-    console.log('Text: ', response.text);
-
-    res.status(200).json({ text });
+    )
+    res.status(200).json({ text })
   } catch (e) {
-    console.error('Error', e);
-    res.status(500).json({ error: e.message });
+    console.error('Error', e.message)
+    res.status(500).json({ error: e.message })
   }
-});
+})
 
 router.post('/complete', async (req, res) => {
   try {
-    const { text, activeChatId } = req.body;
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          content: `Finish my thought (do not include my prompt): ${text}`,
-        },
-      ],
-      temperature: 0.5,
-      max_tokens: 2048,
-    });
-
-    console.log(response.data.choices[0].message.content);
-
-    res.status(200).json({ text: response.data.choices[0].message.content });
+    const response = await openai.createChatCompletion(chatConfig)
+    res.status(200).json({ text: response.data.choices[0].message.content })
   } catch (e) {
-    console.error('Error', e);
-    res.status(500).json({ error: e.message });
+    console.error('Error', e)
+    res.status(500).json({ error: e.message })
   }
-});
+})
 
-export default router;
+export default router
